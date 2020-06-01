05a78a3653ef03a7b789dc98537ce5b85979a531
Gilo <gil.cano@gmail.com>
2020-06-01 13:25:22 -0500

    update slider viewlet

--- src/imtheme/blue/viewlets/sliderviewlet.py
+++ src/imtheme/blue/viewlets/sliderviewlet.py
@@ -1,15 +1,32 @@
 # -*- coding: utf-8 -*-
 
+from plone import api
 from plone.app.layout.viewlets import ViewletBase
 
 
 class SliderViewlet(ViewletBase):
 
     def update(self):
-        self.message = self.get_message()
+        self.sliders = self.get_sliders()
 
-    def get_message(self):
-        return u'My message'
+    def get_sliders(self):
+        headers = []
+        brains = api.content.find(
+            portal_type='SliderContent',
+            path='/infomatem/actividades/headers/',
+            review_state='front-page',
+            sort_on='start'
+        )
+        for b in brains:
+            obj = b.getObject()
+            image = obj.image
+            if image:
+                data = {}
+                data['url'] = obj.urlevent
+                data['urlimage'] = obj.absolute_url() + '/@@images/image'
+                headers.append(data)
+        return headers
+
 
     def render(self):
         return super(SliderViewlet, self).render()
