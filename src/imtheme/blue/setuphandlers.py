40f71a1ea52c98c4959e707b02b63907ebfb847e
Gilo <gil.cano@gmail.com>
2020-06-01 00:24:14 -0500

    post_install script

--- src/imtheme/blue/setuphandlers.py
+++ src/imtheme/blue/setuphandlers.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
 
@@ -14,8 +15,47 @@ class HiddenProfiles(object):
 
 
 def post_install(context):
-    """Post install script"""
+    """Post install script for infomatem migration"""
+    portal = api.portal.get()
+    if portal.id != 'infomatem':
+        return
     # Do something at the end of the installation of this package.
+    avisos = api.content.get('/inicio/1/2/copy_of_avisos/')
+    avisos.setTitle('AVISOS')
+    avisos.reindexObject(idxs=['Title'])
+    intercambio = api.content.get('/inicio/1/2/copy_of_movimiento-academico')
+    intercambio.setTitle('INTERCAMBIO ACADÉMICO')
+    intercambio.reindexObject(idxs=['Title'])
+
+    # Rename menu items
+    act = api.content.get('/actividades')
+    act.setTitle('Actividades académicas')
+    act.reindexObject(idxs=['Title'])
+    servicios = api.content.get('/servicios')
+    servicios.setTitle('Servicios internos')
+    servicios.reindexObject(idxs=['Title'])
+
+    # hide items
+    for name in ['/docencia', '/vinculacion', '/unidad']:
+        item = api.content.get(name)
+        item.setExcludeFromNav(True)
+        item.reindexObject()
+
+    # new content
+    biblio = api.content.create(
+        type='Folder', title='Bibliotecas', container=portal)
+    api.content.transition(obj=biblio, transition='publish_internally')
+    api.content.transition(obj=biblio, transition='publish_externally')
+
+    # sort
+    portal.moveObject('fsd', 2)
+    portal.moveObject('divulgacion', 4)
+    portal.moveObject('actividades', 5)
+    portal.moveObject('seminarios', 6)
+    portal.moveObject('bibliotecas', 7)
+    portal.moveObject('servicios', 8)
+    putils = api.portal.get_tool(name='plone_utils')
+    putils.reindexOnReorder(portal)
 
 
 def uninstall(context):
