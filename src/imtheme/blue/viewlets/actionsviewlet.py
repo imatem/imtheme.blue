a8514559dc58ac8fddf19c9dbff0064a2e56893d
Gilo <gil.cano@gmail.com>
2020-06-02 15:03:57 -0500

    complete actions viewlet. closes #13

--- src/imtheme/blue/viewlets/actionsviewlet.py
+++ src/imtheme/blue/viewlets/actionsviewlet.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from plone import api
 from plone.app.layout.viewlets import ViewletBase
+from zope.component import getMultiAdapter
 
 
 class ActionsViewlet(ViewletBase):
@@ -13,3 +15,12 @@ class ActionsViewlet(ViewletBase):
 
     def render(self):
         return super(ActionsViewlet, self).render()
+
+    def is_anonymous(self):
+        return api.user.is_anonymous()
+
+    def user_actions(self):
+        context_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_context_state')
+        actions = context_state.actions('user')
+        return actions
