4b136b978b51a55f7c46bf017e74b5bb843714b1
Gilo <gil.cano@gmail.com>
2020-06-01 22:34:23 -0500

    slicebox fix

--- src/imtheme/blue/browser/sliderboxtopicview.py
+++ src/imtheme/blue/browser/sliderboxtopicview.py
@@ -1,17 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from imtheme.blue import _
-from Products.Five.browser import BrowserView
+from Products.Collage.browser.views import BaseTopicView
 
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+class SliderBoxTopicView(BaseTopicView):
 
-
-class SliderBoxTopicView(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('sliderboxtopicview.pt')
-
-    def __call__(self):
-        # Implement your own actions:
-        self.msg = _(u'A small message')
-        return self.index()
+    def hasImage(self, obj):
+        if obj.getImage():
+            return True
+        return False
