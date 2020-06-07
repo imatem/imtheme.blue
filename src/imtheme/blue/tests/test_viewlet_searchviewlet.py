99661b51eaeaf2a036baec773599794935f48f08
Gilo <gil.cano@gmail.com>
2020-06-07 13:22:39 -0500

    add search viewlet

---
+++ src/imtheme/blue/tests/test_viewlet_searchviewlet.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from imtheme.blue.interfaces import IImthemeBlueLayer
+from imtheme.blue.testing import IMTHEME_BLUE_FUNCTIONAL_TESTING
+from imtheme.blue.testing import IMTHEME_BLUE_INTEGRATION_TESTING
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from Products.Five.browser import BrowserView
+from zope.component import queryMultiAdapter
+from zope.interface import alsoProvides
+from zope.viewlet.interfaces import IViewletManager
+
+import unittest
+
+
+class ViewletIntegrationTest(unittest.TestCase):
+
+    layer = IMTHEME_BLUE_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.app = self.layer['app']
+        self.request = self.app.REQUEST
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        api.content.create(self.portal, 'Document', 'other-document')
+        api.content.create(self.portal, 'News Item', 'newsitem')
+
+    def test_searchviewlet_is_registered(self):
+        view = BrowserView(self.portal['other-document'], self.request)
+        manager_name = 'plone.portalheader'
+        alsoProvides(self.request, IImthemeBlueLayer)
+        manager = queryMultiAdapter(
+            (self.portal['other-document'], self.request, view),
+            IViewletManager,
+            manager_name,
+            default=None
+        )
+        self.assertIsNotNone(manager)
+        manager.update()
+        my_viewlet = [v for v in manager.viewlets if v.__name__ == 'search-viewlet']  # NOQA: E501
+        self.assertEqual(len(my_viewlet), 1)
+
+
+class ViewletFunctionalTest(unittest.TestCase):
+
+    layer = IMTHEME_BLUE_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
