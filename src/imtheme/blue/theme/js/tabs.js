e4c2b709bdc0e377120c6bcdd054f5164e8534ed
Gilo <gil.cano@gmail.com>
2020-09-20 01:21:17 -0500

    basic function with static titles

--- src/imtheme/blue/theme/js/tabs.js
+++ src/imtheme/blue/theme/js/tabs.js
@@ -1,17 +1,27 @@
+// $(document).ready(function(){
+//   var newWidget="<div class='widget-wrapper'> <ul class='tab-wrapper'></ul> <div class='new-widget'></div></div>";
+//   $(".widget").hide();
+//   $(".widget:first").before(newWidget);
+//   $(".widget > div").each(function(){
+//       $(".tab-wrapper").append("<li class='tab'>"+this.id+"</li>");
+//       $(this).appendTo(".new-widget");
+//   });
+//   $(".tab").click(function(){
+//       $(".new-widget > div").hide();
+//       $('#'+$(this).text()).show();
+//       $(".tab").removeClass("active-tab");
+//       $(this).addClass("active-tab");
+//   });
+//   $(".tab:first").click();
+// });
+
+
 $(document).ready(function(){
-  // var newWidget="<div class='widget-wrapper'> <ul class='tab-wrapper'></ul> <div class='new-widget'></div></div>";
-  var newWidget = $(".widget-wrapper")
-  $(".widget").hide();
-  $(".widget:first").before(newWidget);
-  $(".widget > div").each(function(){
-      // $(".tab-wrapper").append("<li class='tab'>"+this.id+"</li>");
-      $(this).appendTo(".new-widget");
-  });
   $(".tab").click(function(){
-      $(".new-widget > div").hide();
-      $('#tab'+$(this).id).show();
-      $(".tab").removeClass("active-tab");
-      $(this).addClass("active-tab");
-  });
-  $(".tab:first").click();
+          $(".new-widget > div").hide();
+          $('#'+$(this).text()).show();
+          $(".tab").removeClass("active-tab");
+          $(this).addClass("active-tab");
+      });
+      $(".tab:first").click();
 });
