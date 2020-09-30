f47429d25dd458cddd42bd03a41a020cbfc6f81f
Gilo <gil.cano@gmail.com>
2020-09-30 10:52:23 -0500

    proximas activiades carrusel

--- src/imtheme/blue/theme/js/tabs.js
+++ src/imtheme/blue/theme/js/tabs.js
@@ -6,4 +6,21 @@ $(document).ready(function(){
         $(this).addClass("active-tab");
     });
     $(".tab:first").click();
+
+    // carrusel proximas actividades
+    var carrusel_index = 0;
+    showcarrusel();
+
+    function showcarrusel() {
+      var i;
+      var slides = $("#galleria > a");
+      slides.hide();
+      carrusel_index++;
+      if (carrusel_index > slides.length) {
+        carrusel_index = 1;
+      }
+      slides[carrusel_index-1].style.display = "unset";
+      setTimeout(showcarrusel, 8000); // Change image every x seconds
+    }
+
 });
