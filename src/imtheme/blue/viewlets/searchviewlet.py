61fc6bbaf6465c4a709a3c01d08e7bf1af9ac1f3
adriana-rv <adriana.rv@gmail.com>
2020-09-30 20:16:10 -0500

    Add searchbox viewlet

--- src/imtheme/blue/viewlets/searchviewlet.py
+++ src/imtheme/blue/viewlets/searchviewlet.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from plone.app.layout.viewlets import ViewletBase
+from plone.app.layout.viewlets.common import SearchBoxViewlet
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
-class SearchViewlet(ViewletBase):
+class SearchViewlet(SearchBoxViewlet):
 
-    def update(self):
-        self.message = self.get_message()
+    render = ViewPageTemplateFile("searchviewlet.pt")
+
+    # def update(self):
+    #     self.message = self.get_message()
 
     def get_message(self):
         return u'My message'
 
-    def render(self):
-        return super(SearchViewlet, self).render()
+    # def render(self):
+        # return super(SearchViewlet, self).render()
+
