a1f0ec8d7f1d35e60acab6fc4dd4920edc4bca43
Gilo <gil.cano@gmail.com>
2020-10-17 22:22:16 -0500

    fix menu break

--- src/imtheme/blue/theme/js/menu.js
+++ src/imtheme/blue/theme/js/menu.js
@@ -6,3 +6,14 @@ function myFunction() {
     x.className = "supermenu";
   }
 }
+
+// split menu list in two list
+// $(document).ready(function(){
+//   $('#portal-globalnav').filter('ul').each(function() {
+//     var $li = $(this).children(),
+//         $newUl = $('<ul>').insertAfter(this),
+//         middle = $li.length - 4;
+//     $li.filter(':gt(' + middle + ')').appendTo($newUl);
+//     $newUl.addClass("supermenu");
+//   });
+// });
