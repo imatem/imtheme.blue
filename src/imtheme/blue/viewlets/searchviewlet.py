2a8f8b95d514eddbc6146cfa90ec79eeb2864ad4
Gilo <gil.cano@gmail.com>
2021-06-18 01:49:02 -0500

    search in section

--- src/imtheme/blue/viewlets/searchviewlet.py
+++ src/imtheme/blue/viewlets/searchviewlet.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from plone.app.layout.viewlets import ViewletBase
+# from plone.app.layout.viewlets import ViewletBase
+from plone.app.layout.viewlets.common import SearchBoxViewlet
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
-class SearchViewlet(ViewletBase):
 
-    def update(self):
-        self.message = self.get_message()
+class SearchViewlet(SearchBoxViewlet):
 
-    def get_message(self):
-        return u'My message'
+    render = ViewPageTemplateFile("searchviewlet.pt")
 
-    def render(self):
-        return super(SearchViewlet, self).render()
+    # def update(self):
+    #     self.message = self.get_message()
+
+    # def get_message(self):
+    #     return u'My message'
+
+    # def render(self):
+    #     return super(SearchViewlet, self).render()
