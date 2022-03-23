291dd6679a805d8b0422cfa960f163a5cbdae709
Gilo <gil.cano@gmail.com>
2022-03-23 01:46:07 -0600

    prepare menu, add sintax

--- src/imtheme/blue/theme/js/menu.js
+++ src/imtheme/blue/theme/js/menu.js
@@ -1,8 +1,48 @@
-function myFunction() {
-  var x = document.getElementById("portal-globalnav");
-  if (x.className === "supermenu") {
-    x.className += " responsive";
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".supermenu");
+const items = document.querySelectorAll('[id$="-level1"]');
+
+/* Toggle mobile menu */
+function toggleMenu() {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    toggle.querySelector("a").innerHTML = "<i class='fa fa-bars'> <span class='text-menu'>Menú</span></i>";
   } else {
-    x.className = "supermenu";
+    menu.classList.add("active");
+    toggle.querySelector("a").innerHTML = "<i class='fa fa-times'> <span class='text-menu'>Menú</span></i>";
   }
 }
+
+/* Activate Submenu */
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
+}
+
+/* Close Submenu From Anywhere */
+function closeSubmenu(e) {
+  let isClickInside = menu.contains(e.target);
+
+  if (!isClickInside && menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+  }
+}
+/* Event Listeners */
+toggle.addEventListener("click", toggleMenu, false);
+
+$('.supermenu a').on('click','.nav-plus',function(){
+  if($(this).hasClass('nav-minus') == false){
+      $(this).parent('a').parent('li').find('> ul').addClass("submenu-active");
+      $(this).addClass('nav-minus');
+    }else{
+      $(this).parent('a').parent('li').find('> ul').removeClass("submenu-active");
+      $(this).removeClass('nav-minus');
+  }
+  return false;
+});
